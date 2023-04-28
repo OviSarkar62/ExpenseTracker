37b2001ffbefe37ff15736ad3050df0db245e7f7
Ovi Sarkar <72793142+OviSarkar62@users.noreply.github.com>
2023-04-28 11:44:43 +0600

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      await axios.post("https://expense-tracker-n12j.onrender.com/users/register", values);
+      await axios.post("/api/v1/users/register", values);
       message.success("Registration Successful");
       setLoading(false);
       navigate("/login");
