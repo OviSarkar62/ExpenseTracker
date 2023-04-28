b21ae03e4ed06d9cc11dc88a8bb81ad8417dfc36
Ovi Sarkar <72793142+OviSarkar62@users.noreply.github.com>
2023-04-28 11:39:58 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
+const path = require("path");
 const connectDb = require("./config/connectDb");
 // config dot env file
 dotenv.config();
@@ -23,6 +24,14 @@ app.use(cors());
 app.use("/api/v1/users", require("./routes/userRoute"));
 //transactions routes
 app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
+
+// static files
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
+
 //port
 const PORT = process.env.PORT || 4001;
 
